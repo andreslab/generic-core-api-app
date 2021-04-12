be6ff282d04e310d07058fea17d46ac86a57b11b
Jaime Andrade <jaimeandrade454@gmail.com>
2021-04-12 15:38:23 -0700

    upload file with path api/upload/

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { indexController } from "../controller/indexController";
+import { upload, uploadFileController } from "../controller/uploadFileController";
 const router: Router = Router();
 
 router.post('/payment', indexController.processPayment);
 router.post('/verify', indexController.verifyCheck);
+router.post('/upload', upload.single('productImage'), uploadFileController.uploadFile);
 
 export default router;
